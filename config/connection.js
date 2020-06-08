fcf330a6b8ec5048cc2e33a13ed71943da22b452
rawanfatima <rawanfatima@outlook.com>
2020-06-07 22:59:07 -0700

    final edits

--- config/connection.js
+++ config/connection.js
@@ -7,8 +7,6 @@ const sequelize = process.env.JAWDB_URL
 ? new Sequelize(process.env.JAWDB_URL)
 : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
-    user: 'me',
-    password: 'sqlpassword',
     dialect: 'mysql',
     dialectOptions: {
         decimalNumbers: true,
--- server.js
+++ server.js
@@ -1,1 +1,21 @@
 const port = process.env.PORT;
+const express = require('express');
+const routes = require('./routes');
+// import sequelize connection
+const sequelize = require('./config/connection.js');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+// sync sequelize models to the database, then turn on the server
+// app.listen(PORT, () => {
+//   console.log(`App listening on port ${PORT}!`);
+// });
+sequelize.sync({ force: false }).then(() => { //once you have the association in index.js file , change the force = true and start the server to drop and create the tables. Once thats done, change it back to false
+  app.listen(PORT, () => console.log('Now listening'));
+});
